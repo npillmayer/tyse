56f3103be7e104121fe415465c9a4493e3179da7
Norbert Pillmayer <norbert@pillmayer.com>
2021-01-19 08:45:06 +0100

    Re-organizing

--- core/locate/locator.go
+++ core/locate/locator.go
@@ -1,4 +1,6 @@
 /*
+Wait for the new filesystem API planned by a Go proposal (from the core team).
+
 This is currentyl just a stand-in for a real implementation.
 
 It grows whenever I add some functionality needed for tests. Everything here
